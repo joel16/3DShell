ab402fed29057b78ebb767f41548f80a5cd1d556
Joel16 <sauskethunderkid@gmail.com>
2018-03-12 00:40:05 -0500

    More gallery improvments
    - Added support for Nintendo (both screen) screenshots.
    - Added support for Nintendo camera images.
    - Added support for wallpapers up to 800 * 480. (Some images may appear badly stretched due to the dimensions - but that depends on the image)
    - Speed up gallery bar animation

--- source/menus/menu_gallery.c
+++ source/menus/menu_gallery.c
@@ -9,6 +9,12 @@
 #include "textures.h"
 #include "utils.h"
 
+#define DIMENSION_DEFAULT             0
+#define DIMENSION_NINTENDO_SCREENSHOT 1
+#define DIMENSION_NINTENDO_PICTURE    2
+#define DIMENSION_3DSHELL_SCREENSHOT  3
+#define DIMENSION_OTHER               4
+
 #define GALLERY_BAR_Y_BOUNDARY  -35
 #define GALLERY_NAME_Y_BOUNDARY -11
 
@@ -29,10 +35,10 @@ void Gallery_DisplayImage(char * path)
 
 	u64 start = osGetTime();
 
-	bool bothScreens = false;
-
 	float width = pp2d_get_texture_width(TEXTURE_GALLERY_IMAGE);
 	float height = pp2d_get_texture_height(TEXTURE_GALLERY_IMAGE);
+
+	int dimensions = 0;
 
 	while (aptMainLoop())
 	{
@@ -45,8 +51,16 @@ void Gallery_DisplayImage(char * path)
 
 			pp2d_draw_rectangle(0, 0, 320, 240, RGBA8(33, 39, 43, 255));
 
-			if (bothScreens)
-				pp2d_draw_texture_part(TEXTURE_GALLERY_IMAGE, 0, 0, 40, 240, 320, 240);
+			switch (dimensions)
+			{
+				case DIMENSION_NINTENDO_SCREENSHOT:
+					pp2d_draw_texture_part(TEXTURE_GALLERY_IMAGE, 0, 0, 56, 272, 320, 240);
+					break;
+
+				case DIMENSION_3DSHELL_SCREENSHOT:
+					pp2d_draw_texture_part(TEXTURE_GALLERY_IMAGE, 0, 0, 40, 240, 320, 240);
+					break;
+			}
 		
 		pp2d_end_draw();
 
@@ -55,17 +69,43 @@ void Gallery_DisplayImage(char * path)
 			pp2d_draw_rectangle(0, 0, 400, 240, RGBA8(33, 39, 43, 255));
 
 			if ((width <= 400.0) && (height <= 240.0))
-				pp2d_draw_texture(TEXTURE_GALLERY_IMAGE, ((400.0 - width) / 2.0), ((240.0 - height) / 2.0));
-			else if ((width >= 400.0) && (height >= 480.0)) // Both screens
+				DIMENSION_DEFAULT;
+			else if ((width == 432) && (height == 528)) // Nintnedo's screenshot (both screens) dimensions.
+				dimensions = DIMENSION_NINTENDO_SCREENSHOT;
+			else if ((width == 640) && (height == 480)) // Nintnedo's CAM dimensions.
+				dimensions = DIMENSION_NINTENDO_PICTURE;
+			else if ((width == 400) && (height == 480))
+				dimensions = DIMENSION_3DSHELL_SCREENSHOT;
+			else if ((width > 400.0) && (height > 240.0))
+				dimensions = DIMENSION_OTHER;
+
+			switch (dimensions)
 			{
-				pp2d_draw_texture(TEXTURE_GALLERY_IMAGE, ((400.0 - width) / 2.0), 0);
-				bothScreens = true;
+				case DIMENSION_DEFAULT:
+					pp2d_draw_texture(TEXTURE_GALLERY_IMAGE, ((400.0 - width) / 2.0), ((240.0 - height) / 2.0));
+					break;
+
+				case DIMENSION_NINTENDO_SCREENSHOT:
+					pp2d_draw_texture_part(TEXTURE_GALLERY_IMAGE, 0, 0, 16, 16, 400, 240);
+					break;
+
+				case DIMENSION_NINTENDO_PICTURE:
+					pp2d_draw_texture_scale(TEXTURE_GALLERY_IMAGE, 40, 0, 0.5, 0.5);
+					break;
+
+				case DIMENSION_3DSHELL_SCREENSHOT:
+					pp2d_draw_texture_part(TEXTURE_GALLERY_IMAGE, 0, 0, 0, 0, 400, 240);
+					break;
+
+				case DIMENSION_OTHER:
+					pp2d_draw_texture_scale(TEXTURE_GALLERY_IMAGE, 0, 0, 400.0/width, 240.0/height);
+					break;
 			}
 
-			if (osGetTime() - start >= (2000))
+			if (osGetTime() - start >= (1500))
 			{
-				nameY -= 3;
-				galleryBarY -= 3;
+				nameY -= 4;
+				galleryBarY -= 4;
 			}
 
 			if (galleryBarY == -35)
