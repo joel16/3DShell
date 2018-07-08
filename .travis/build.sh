8e6d235ac000df866db07d542fcaa626cc9d8bc4
Joel16 <sauskethunderkid@gmail.com>
2018-07-08 15:59:43 -0500

    Remove .elf and .smdh

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,5 +3,5 @@
 source /etc/profile.d/devkit-env.sh
 cd /3DShell
 make clean && make
-rm .gitignore .gitmodules .travis.yml LICENSE Makefile README.md
+rm .gitignore .gitmodules .travis.yml 3DShell.elf 3DShell.smdh LICENSE Makefile README.md
 rm -rf .travis build include res romfs source
