1d232b889c794d6961eec0332d6f71c65bbf6716
Joel16 <sauskethunderkid@gmail.com>
2018-07-08 15:54:04 -0500

    Try to build properly 2

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 source /etc/profile.d/devkit-env.sh
-
+cd /3DShell
 make clean && make
 rm .gitignore .gitmodules .travis.yml LICENSE Makefile README.md
 rm -rf .travis build include res romfs source
