4632b0a25c80be6b6ba4debe3765309f6cda41a4
Joel16 <sauskethunderkid@gmail.com>
2018-07-17 18:22:04 -0500

    Attempt to fix travis builds again

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,5 +3,5 @@
 source /etc/profile.d/devkit-env.sh
 cd /3DShell
 make clean && make
-rm .gitignore .gitmodules .travis.yml 3DShell.elf 3DShell.smdh LICENSE Makefile README.md
+rm .gitignore .gitmodules .travis.yml 3DShell.elf 3DShell.smdh devkitpro-pacman.deb LICENSE Makefile README.md
 rm -rf .travis build include res romfs source
