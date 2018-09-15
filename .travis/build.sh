0ff0189544ed9b0177735419d73f4b0cde4738ba
Joel <sauskethunderkid@gmail.com>
2018-09-15 12:17:59 -0500

    Update build.sh

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,6 +2,7 @@
 
 source /etc/profile.d/devkit-env.sh
 cd /3DShell
+git submodule foreach git pull origin master
 make clean && make
 rm .gitignore .gitmodules .travis.yml 3DShell.elf 3DShell.smdh devkitpro-pacman.deb LICENSE Makefile README.md
 rm -rf .travis build include res romfs source
