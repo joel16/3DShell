61170291bbc8eb3126c08a2afb93ec2d5c66f687
Joel16 <sauskethunderkid@gmail.com>
2019-01-01 23:15:25 -0600

    Add ability to zoom into image if < 400 * 240
    Also update lodepng

--- source/menus/menu_gallery.c
+++ source/menus/menu_gallery.c
@@ -5,7 +5,6 @@
 #include "common.h"
 #include "config.h"
 #include "fs.h"
-#include "menu_gallery.h"
 
 #define LOADBMP_IMPLEMENTATION
 #include "loadbmp.h"
@@ -27,7 +26,7 @@
 #define DIMENSION_OTHER               4
 
 static char album[512][512];
-static int count = 0, selection = 0, dimensions = 0;
+static int count = 0, selection = 0, dimensions = 0, pos_x = 0, pos_y = 0;
 C2D_Image image;
 
 // Thanks to LiquidFenrir
@@ -192,7 +191,7 @@ static void Gallery_FreeImage(C2D_Image *image) {
 
 static bool Gallery_DrawImage(C2D_Image image, float x, float y, float start, float end, float scaleX, float scaleY) {
     C2D_DrawParams params = {
-        { x, y, scaleX * image.subtex->width, scaleY * image.subtex->height },
+        { x - (pos_x * scaleX - pos_x) / 2, y - (pos_y * scaleY - pos_y) / 2, scaleX * image.subtex->width, scaleY * image.subtex->height },
         { start, end },
         0.5f, 0.0f
     };
@@ -269,7 +268,18 @@ void Gallery_DisplayImage(char *path) {
     Gallery_GetImageList();
     Gallery_LoadTexture(path);
 
+    float zoom_factor = 1.0f;
+
+    u64 last_time = osGetTime(), current_time = 0;
+
+    pos_x = 0;
+    pos_y = 0;
+
     while(aptMainLoop()) {
+        current_time = osGetTime();
+        u64 delta_time = current_time - last_time;
+        last_time = current_time;
+
         C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
         C2D_TargetClear(RENDER_TOP, C2D_Color32(33, 39, 43, 255));
         C2D_TargetClear(RENDER_BOTTOM, C2D_Color32(33, 39, 43, 255));
@@ -277,7 +287,8 @@ void Gallery_DisplayImage(char *path) {
 
         switch (dimensions) {
             case DIMENSION_DEFAULT:
-                Gallery_DrawImage(image, ((400.0f - image.subtex->width) / 2.0f), ((240.0f - image.subtex->height) / 2.0f), 0, 0, 1.0f, 1.0f);
+                Gallery_DrawImage(image, ((400.0f - (image.subtex->width * zoom_factor)) / 2.0f), ((240.0f - (image.subtex->height * zoom_factor)) / 2.0f), 
+                    0, 0, 1.0f * zoom_factor, 1.0f * zoom_factor);
                 break;
 
             case DIMENSION_NINTENDO_SCREENSHOT:
@@ -293,7 +304,10 @@ void Gallery_DisplayImage(char *path) {
                 break;
 
             case DIMENSION_OTHER:
-                Gallery_DrawImage(image, 0, 0, 0, 0, 400.0f / image.subtex->width, 240.0f / image.subtex->height);
+                Gallery_DrawImage(image, 0, 0, 0, 0, 400.0f / (image.subtex->width), 240.0f / (image.subtex->height));
+                break;
+
+            default:
                 break;
         }
 
@@ -307,15 +321,57 @@ void Gallery_DisplayImage(char *path) {
             case DIMENSION_3DSHELL_SCREENSHOT:
                 Gallery_DrawImage(image, 0, 0, 40, 240, 1.0f, 1.0f);
                 break;
+
+            default:
+                break;
         }
 
         hidScanInput();
         u32 kDown = hidKeysDown();
+        u32 kHeld = hidKeysHeld();
 
-        if ((kDown & KEY_LEFT) || (kDown & KEY_L))
+        if ((kDown & KEY_DLEFT) || (kDown & KEY_L))
             Gallery_HandleNext(false);
-        else if ((kDown & KEY_RIGHT) || (kDown & KEY_R))
+        else if ((kDown & KEY_DRIGHT) || (kDown & KEY_R))
             Gallery_HandleNext(true);
+
+        if (dimensions == DIMENSION_DEFAULT) {
+            if ((image.subtex->height * zoom_factor > 320) || (image.subtex->width * zoom_factor > 400)) {
+                double velocity = 2 / zoom_factor;
+
+                if (kHeld & KEY_CPAD_UP)
+                    pos_y -= ((velocity * zoom_factor) * delta_time);
+                else if (kHeld & KEY_CPAD_DOWN)
+                    pos_y += ((velocity * zoom_factor) * delta_time);
+                else if (kHeld & KEY_CPAD_LEFT)
+                    pos_x -= ((velocity * zoom_factor) * delta_time);
+                else if (kHeld & KEY_CPAD_RIGHT)
+                    pos_x += ((velocity * zoom_factor) * delta_time);
+            }
+
+            if ((kHeld & KEY_DUP) || (kHeld & KEY_CSTICK_UP)) {
+                zoom_factor += 0.5f * (delta_time * 0.001);
+
+                if (zoom_factor > 2.0f)
+                    zoom_factor = 2.0f;
+            }
+            else if ((kHeld & KEY_DDOWN) || (kHeld & KEY_CSTICK_DOWN)) {
+                zoom_factor -= 0.5f * (delta_time * 0.001);
+
+                if (zoom_factor < 0.5f)
+                    zoom_factor = 0.5f;
+
+                if (zoom_factor <= 1.0f) {
+                    pos_x = 0;
+                    pos_y = 0;
+                }
+            }
+        }
+
+        Utils_SetMax(&pos_x, image.subtex->width, image.subtex->width);
+        Utils_SetMin(&pos_x, -image.subtex->width, -image.subtex->width);
+        Utils_SetMax(&pos_y, image.subtex->height, image.subtex->height);
+        Utils_SetMin(&pos_y, -image.subtex->height, -image.subtex->height);
 
         Draw_EndFrame();
         
