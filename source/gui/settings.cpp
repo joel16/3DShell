c89319f8e77fa97f97ad3e2ab9a3d29770e33292
Joel16 <joel16@iastate.edu>
2021-03-22 13:58:38 -0400

    touch: Expand touch controls to settings/updates

--- source/gui/settings.cpp
+++ source/gui/settings.cpp
@@ -7,6 +7,7 @@
 #include "gui.h"
 #include "net.h"
 #include "textures.h"
+#include "touch.h"
 #include "utils.h"
 
 namespace GUI {
@@ -23,7 +24,7 @@ namespace GUI {
     static bool network_status = false, update_available = false, update_popup = false;
 
     static void DisplaySortSettings(void) {
-        C2D::Text(10, 28, 0.44f, WHITE, "Sorting Options");
+        C2D::Text(35, 30, 0.44f, WHITE, "Sorting Options");
         
         C2D::Text(10, 58, 0.44f, cfg.dark_theme? WHITE : BLACK, "Alphabetical");
         C2D::Text(10, 74, 0.42f, cfg.dark_theme? WHITE : BLACK, "Sort alphabetically in ascending order.");
@@ -54,12 +55,55 @@ namespace GUI {
             selection = 0;
             settings_state = GENERAL_SETTINGS;
         }
+        
+        if (Touch::Rect(0, 55, 320, 94)) {
+            selection = 0;
+            
+            if (*kDown & KEY_TOUCH) {
+                cfg.sort = selection;
+                Config::Save(cfg);
+                FS::GetDirList(cfg.cwd, item->entries);
+            }
+        }
+        else if (Touch::Rect(0, 95, 320, 134)) {
+            selection = 1;
+            
+            if (*kDown & KEY_TOUCH) {
+                cfg.sort = selection;
+                Config::Save(cfg);
+                FS::GetDirList(cfg.cwd, item->entries);
+            }
+        }
+        else if (Touch::Rect(0, 135, 320, 174)) {
+            selection = 2;
+            
+            if (*kDown & KEY_TOUCH) {
+                cfg.sort = selection;
+                Config::Save(cfg);
+                FS::GetDirList(cfg.cwd, item->entries);
+            }
+        }
+        else if (Touch::Rect(0, 175, 320, 215)) {
+            selection = 3;
+            
+            if (*kDown & KEY_TOUCH) {
+                cfg.sort = selection;
+                Config::Save(cfg);
+                FS::GetDirList(cfg.cwd, item->entries);
+            }
+        }
+        else if (Touch::Rect(5, 25, 30, 50)) {
+            if (*kDown & KEY_TOUCH) {
+                selection = 0;
+                settings_state = GENERAL_SETTINGS;
+            }
+        }
 
         Utils::SetBounds(&selection, 0, 3);
     }
 
     static void DisplayUpdateSettings(void) {
-        C2D::Text(10, 28, 0.44f, WHITE, "Updates");
+        C2D::Text(35, 30, 0.44f, WHITE, "Updates");
 
         C2D::Text(10, 58, 0.44f, cfg.dark_theme? WHITE : BLACK, "Check for updates");
         C2D::Text(10, 74, 0.42f, cfg.dark_theme? WHITE : BLACK, "Downloads and installs the latest version.");
@@ -97,10 +141,29 @@ namespace GUI {
             
             Utils::SetBounds(&selection, 0, 2);
         }
+
+        if (Touch::Rect(0, 55, 320, 94)) {
+            selection = 0;
+            
+            if (*kDown & KEY_TOUCH) {
+                Net::Init();
+                network_status =  Net::GetNetworkStatus();
+                tag_name = Net::GetLatestReleaseJSON();
+                update_available = Net::GetAvailableUpdate(tag_name);
+                update_popup = true;
+                Net::Exit();
+            }
+        }
+        else if (Touch::Rect(5, 25, 30, 50)) {
+            if (*kDown & KEY_TOUCH) {
+                selection = 0;
+                settings_state = GENERAL_SETTINGS;
+            }
+        }
     }
 
     static void DisplayGeneralSettings(void) {
-        C2D::Text(10, 28, 0.44f, WHITE, "Settings");
+        C2D::Text(10, 30, 0.44f, WHITE, "Settings");
 
         C2D::Text(10, 58, 0.44f, cfg.dark_theme? WHITE : BLACK, "Sort by");
         C2D::Text(10, 74, 0.42f, cfg.dark_theme? WHITE : BLACK, "Select between various sorting options.");
@@ -151,12 +214,47 @@ namespace GUI {
         else if (*kDown & KEY_B)
             item->state = MENU_STATE_FILEBROWSER;
 
+        if (Touch::Rect(0, 55, 320, 94)) {
+            selection = 0;
+            
+            if (*kDown & KEY_TOUCH) {
+                settings_state = SORT_SETTINGS;
+                selection = 0;
+            }
+        }
+        else if (Touch::Rect(0, 95, 320, 134)) {
+            selection = 1;
+            
+            if (*kDown & KEY_TOUCH) {
+                cfg.dark_theme = !cfg.dark_theme;
+                Config::Save(cfg);
+            }
+        }
+        else if (Touch::Rect(0, 135, 320, 174)) {
+            selection = 2;
+            
+            if (*kDown & KEY_TOUCH) {
+                cfg.dev_options = !cfg.dev_options;
+                Config::Save(cfg);
+            }
+        }
+        else if (Touch::Rect(0, 175, 320, 215)) {
+            selection = 3;
+            
+            if (*kDown & KEY_TOUCH) {
+                settings_state = UPDATE_SETTINGS;
+                selection = 0;
+            }
+        }
+
         Utils::SetBounds(&selection, 0, 3);
     }
 
     void DisplaySettings(MenuItem *item) {
         C2D::Rect(0, 20, 400, 35, cfg.dark_theme? MENU_BAR_DARK : STATUS_BAR_LIGHT); // Menu bar
         C2D::Rect(0, 55, 320, 185, cfg.dark_theme? BLACK_BG : WHITE);
+        if (settings_state != GENERAL_SETTINGS)
+            C2D::Image(icon_back, 5, 25);
 
         C2D::Rect(0, 55 + (selection * sel_dist), 320, sel_dist, cfg.dark_theme? SELECTOR_COLOUR_DARK : SELECTOR_COLOUR_LIGHT);
 
--- source/gui/update.cpp
+++ source/gui/update.cpp
@@ -10,7 +10,10 @@
 #include "log.h"
 #include "net.h"
 #include "textures.h"
+#include "touch.h"
 #include "utils.h"
+
+static float cancel_height = 0.f, cancel_width = 0.f, confirm_height = 0.f, confirm_width = 0.f, text_width = 0.f;
 
 // Kinda messy, perhaps clean this up later if I ever revisit this.
 namespace GUI {
@@ -19,8 +22,33 @@ namespace GUI {
     static const std::string prompt = "Do you wish to download and install vX.X.X?";
     static const std::string success = "Update installed. Please re-run the application";
     static const std::string no_updates = "You are already on the latest version";
-    static float cancel_height = 0.f, cancel_width = 0.f, confirm_height = 0.f, confirm_width = 0.f, text_width = 0.f;
     static bool done = false;
+
+    void DownloadHelper(const std::string &tag) {
+        Net::Init();
+        s32 prio = 0;
+        download_progress = true;
+        svcGetThreadPriority(&prio, CUR_THREAD_HANDLE);
+        Thread thread = threadCreate(GUI::DownloadProgressBar, nullptr, 32 * 1024, prio - 1, -2, false);
+        Net::GetLatestRelease(tag);
+        download_progress = false;
+        threadJoin(thread, U64_MAX);
+        threadFree(thread);
+        Net::Exit();
+        
+        if (envIsHomebrew()) {
+            Result ret = 0;
+            if (R_FAILED(ret = FSUSER_DeleteFile(sdmc_archive, fsMakePath(PATH_ASCII, __application_path__.c_str()))))
+                Log::Error("FSUSER_DeleteFile(%s) failed: 0x%x\n", __application_path__, ret);
+                
+            if (R_FAILED(ret = FSUSER_RenameFile(sdmc_archive, fsMakePath(PATH_ASCII, "/3ds/3DShell/3DShell_UPDATE.3dsx"), sdmc_archive, fsMakePath(PATH_ASCII, __application_path__.c_str()))))
+                Log::Error("FSUSER_RenameFile(update) failed: 0x%x\n", ret);
+        }
+        else
+            CIA::InstallUpdate();
+        
+        done = true;
+    }
 
     void DisplayUpdateOptions(bool *connection_status, bool *available, const std::string &tag) {
         C2D::Image(cfg.dark_theme? dialog_dark : dialog, ((320 - (dialog.subtex->width)) / 2), ((240 - (dialog.subtex->height)) / 2));
@@ -68,31 +96,8 @@ namespace GUI {
             selection--;
 
         if (*kDown & KEY_A) {
-            if (selection == 1) {
-                Net::Init();
-                s32 prio = 0;
-                download_progress = true;
-                svcGetThreadPriority(&prio, CUR_THREAD_HANDLE);
-                Thread thread = threadCreate(GUI::DownloadProgressBar, nullptr, 32 * 1024, prio - 1, -2, false);
-                Net::GetLatestRelease(tag);
-                download_progress = false;
-                threadJoin(thread, U64_MAX);
-                threadFree(thread);
-                Net::Exit();
-
-                if (envIsHomebrew()) {
-                    Result ret = 0;
-                    if (R_FAILED(ret = FSUSER_DeleteFile(sdmc_archive, fsMakePath(PATH_ASCII, __application_path__.c_str()))))
-                        Log::Error("FSUSER_DeleteFile(%s) failed: 0x%x\n", __application_path__, ret);
-                    
-                    if (R_FAILED(ret = FSUSER_RenameFile(sdmc_archive, fsMakePath(PATH_ASCII, "/3ds/3DShell/3DShell_UPDATE.3dsx"), sdmc_archive, fsMakePath(PATH_ASCII, __application_path__.c_str()))))
-                        Log::Error("FSUSER_RenameFile(update) failed: 0x%x\n", ret);
-                }
-                else
-                    CIA::InstallUpdate();
-
-                done = true;
-            }
+            if (selection == 1)
+                GUI::DownloadHelper(tag);
             else if (!done) {
                 *state = false;
                 item->state = MENU_STATE_SETTINGS;
@@ -104,6 +109,25 @@ namespace GUI {
         else if (*kDown & KEY_B) {
             *state = false;
             item->state = MENU_STATE_SETTINGS;
+        }
+
+        if (Touch::Rect((288 - cancel_width) - 5, (159 - cancel_height) - 5, ((288 - cancel_width) - 5) + cancel_width + 10, ((159 - cancel_height) - 5) + cancel_height + 10)) {
+            selection = 0;
+            
+            if (*kDown & KEY_TOUCH) {
+                if (!done) {
+                    *state = false;
+                    item->state = MENU_STATE_SETTINGS;
+                }
+                else if (done)
+                    longjmp(exit_jmp, 1);
+            }
+        }
+        else if (Touch::Rect((248 - (confirm_width)) - 5, (159 - confirm_height) - 5, ((248 - (confirm_width)) - 5) + confirm_width + 10, ((159 - confirm_height) - 5) + confirm_height + 10)) {
+            selection = 1;
+            
+            if ((*kDown & KEY_TOUCH) && (!done) && (*connection_status) && (*available))
+                GUI::DownloadHelper(tag);
         }
 
         if ((done) || (!*connection_status) || (!*available))
